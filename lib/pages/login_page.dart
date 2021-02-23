5b38ff54777312351eac26a65152030d51ebf60a
developergaurav77 <developer.gaurav77@gmail.com>
2021-02-23 14:40:46 +0545

    day4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,7 +6,6 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Image.asset(
           'assets/images/login.png',
@@ -14,15 +13,43 @@ class LoginPage extends StatelessWidget {
         ),
         Center(
           child: Text(
-            "Login page",
-            style: GoogleFonts.peralta(
+            "WELCOME",
+            style: GoogleFonts.lato(
               color: Colors.indigoAccent,
               fontWeight: FontWeight.bold,
-              fontSize: 22,
+              fontSize: 26,
               letterSpacing: 8.2,
             ),
           ),
         ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 16.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelText: "UserName",
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 16.0),
+          child: TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: "Password",
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        RaisedButton(
+          onPressed: () {},
+          child: Text(
+            "Login",
+            style: TextStyle(color: Colors.white, fontSize: 16.0),
+          ),
+          color: Colors.indigoAccent,
+        )
       ],
     ));
   }
